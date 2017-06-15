f4a1f1142119ff8798a765e69eb18fc86b5b3a15
Mike Klimek <m.klimek@tu-bs.de>
2017-06-16 01:24:34 +0200

    Update ids of all controls to be more descriptive

--- app/js/frontend.js
+++ app/js/frontend.js
@@ -14,73 +14,65 @@ import volume from './volume'
 import curtain from './curtain'
 import lights from './lights'
 
-$(() => {
-  var playbackState = playback.PAUSE
-  const connectionMessage = 'Verbinde mit Server…'
-  const playPauseButton = $('#play_pause')
-  const playPauseGlyph = $('#play_pause_glyph')
-  const stopButton = $('#stop')
-  const curtainToggle = $('#curtain')
-  const volumeSlider = $('volume')
+var playbackState = playback.PAUSE
+const connectionMessage = 'Verbinde mit Server…'
 
+$(() => {
   initApiConnection()
   initPlaybackControl()
   initVolumeControl()
   initLightControl()
   initCurtainControl()
-
-  function initApiConnection() {
-    connectingDialog.show(connectionMessage)
-    apiConnection.onDisconnected(() => connectingDialog.show(connectionMessage))
-    apiConnection.onConnected(() => connectingDialog.hide())
-    apiConnection.connect()
-  }
-
-  function initPlaybackControl() {
-    playPauseButton.click(() => {
-      if (playbackState === playback.PLAY) {
-        playback.pause()
-      } else {
-        playback.play()
-      }
-    })
-    stopButton.click(() => {
-      playback.stop()
-    })
-    playback.onPlaybackChanged((state) => {
-      playbackState = state
-      if (playbackState === playback.PLAY) {
-        playPauseGlyph.addClass('glyphicon-play')
-        playPauseGlyph.removeClass('glyphicon-pause')
-      } else {
-        playPauseGlyph.addClass('glyphicon-pause')
-        playPauseGlyph.removeClass('glyphicon-play')
-      }
-    })
-  }
-
-  function initVolumeControl() {
-
-  }
-
-  function initLightControl() {
-    for (let i in [0, 33, 66, 100]) {
-      const button = $('#light_' + i)
-      button.click(() => {
-        lights.setLightLevel(i)
-      })
-    }
-  }
-
-  function initCurtainControl() {
-    curtainToggle.click(() => {
-      curtainToggle.prop('checked') ? curtain.open() : curtain.close()
-      curtain.onOpened(() => {
-        curtainToggle.bootstrapToggle('on')
-      })
-      curtain.onClosed(() => {
-        curtainToggle.bootstrapToggle('off')
-      })
-    })
-  }
 })
+
+function initApiConnection() {
+  connectingDialog.show(connectionMessage)
+  apiConnection.onDisconnected(() => connectingDialog.show(connectionMessage))
+  apiConnection.onConnected(() => connectingDialog.hide())
+  apiConnection.connect()
+}
+
+function initPlaybackControl() {
+  $('#play-pause-button').click(() => {
+    playbackState === playback.PLAY ? playback.pause() : playback.play()
+  })
+
+  $('#stop-button').click(() => {
+    playback.stop()
+  })
+
+  playback.onPlaybackChanged((state) => {
+    playbackState = state
+    if (playbackState === playback.PLAY) {
+      $('#play-pause-glyph').addClass('glyphicon-pause').removeClass('glyphicon-play')
+    } else {
+      $('#play-pause-glyph').addClass('glyphicon-play').removeClass('glyphicon-pause')
+    }
+  })
+}
+
+function initVolumeControl() {
+  // TODO
+}
+
+function initLightControl() {
+  for (let i in [0, 33, 66, 100]) {
+    $('#light-button-' + i).click(() => {
+      lights.setLightLevel(i)
+    })
+  }
+}
+
+function initCurtainControl() {
+  $('#curtain-switch').change(() => {
+    $('#curtain-switch').prop('checked') ? curtain.open() : curtain.close()
+  })
+
+  curtain.onOpened(() => {
+    $('#curtain-switch').bootstrapToggle('on')
+  })
+
+  curtain.onClosed(() => {
+    $('#curtain-switch').bootstrapToggle('off')
+  })
+}
