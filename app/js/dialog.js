15ea4447845c1ecc76edf1d2eb9ab107f23a0635
Mike Klimek <m.klimek@tu-bs.de>
2017-06-15 20:11:05 +0200

    Add module for dialog with progressbar

---
+++ app/js/dialog.js
@@ -0,0 +1,21 @@
+const dialog = $(
+  '<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
+  '<div class="modal-dialog modal-m">' +
+  '<div class="modal-content">' +
+  '<div class="modal-header"><h3 style="margin:0;"></h3></div>' +
+  '<div class="modal-body">' +
+  '<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
+  '</div>' +
+  '</div></div></div>');
+  
+module.exports = {
+  show: function(message) {
+    dialog.find('.progress-bar').attr('class', 'progress-bar');
+    dialog.find('h3').text(message);
+    dialog.modal();
+  },
+
+  hide: function() {
+    dialog.modal('hide');
+  }
+}
