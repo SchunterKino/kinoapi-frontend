9d2374094616b64000f8a40682725baa818591de
Mike Klimek <m.klimek@tu-bs.de>
2017-06-15 15:09:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# kinoapi-frontend
+
+Webapp to control cinema tech:
+- start/pause the projector
+- control the volume
+- turn on/off lights
+- open/close curtain
+
+
+## Building
+
+Download dependencies:
+```sh
+$ npm install
+```
+
+Build production (minified):
+```sh
+$ npm run build
+```
+
+Automatically build on save:
+```sh
+$ npm run watch
+```
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './app/js/frontend',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'frontend.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Kino Frontend',
+            template: './app/html/frontend.ejs',
+            filename: 'frontend.html'
+        }),
+        new ExtractTextPlugin("styles.css"),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+    ],
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/,
+                loader: 'url-loader'
+            },
+        ]
+    }
+};
