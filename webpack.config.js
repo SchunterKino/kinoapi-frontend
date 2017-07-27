cde0d6a7992da7e044475d32ddfe409a39e22967
Mike Klimek <m.klimek@tu-bs.de>
2017-07-27 23:31:27 +0200

    Add browser prefixes to css when building

--- webpack.config.js
+++ webpack.config.js
@@ -4,42 +4,47 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: './app/js/frontend',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'frontend.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'SchunterKino Fernbedienung',
-            template: './app/html/frontend.ejs',
-            favicon: './app/favicon.gif',
-            filename: 'frontend.html'
-        }),
-        new ExtractTextPlugin("styles.css"),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        }),
-    ],
-    module: {
-        loaders: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
-            },
-            {
-                test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot|.svg$/,
-                loader: 'url-loader'
-            },
-        ]
-    }
+  entry: './app/js/frontend',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'frontend.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'SchunterKino Fernbedienung',
+      template: './app/html/frontend.ejs',
+      favicon: './app/favicon.gif',
+      filename: 'frontend.html'
+    }),
+    new ExtractTextPlugin("styles.css"),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+  ],
+  module: {
+    loaders: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+          use: [
+          'css-loader?importLoaders=1!',
+          {
+            loader: 'postcss-loader',
+            options: {plugins: [ require('autoprefixer') ]}
+          }]
+        })
+      },
+      {
+        test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot|.svg$/,
+        loader: 'url-loader'
+      }
+    ]
+  }
 };
