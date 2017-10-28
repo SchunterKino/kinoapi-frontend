936a94dec7f13c84ddb2d48b345d7a824540c8d4
Mike Klimek <m.klimek@tu-bs.de>
2017-10-28 13:39:13 +0200

    Add linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-const path = require('path');
-const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const WebpackPwaManifest = require('webpack-pwa-manifest')
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require('autoprefixer')
 
 module.exports = {
   entry: './app/js/frontend',
@@ -14,14 +13,12 @@ module.exports = {
   },
   devtool: 'source-map',
   devServer: {
-    contentBase: './dist',
     index: 'frontend.html'
   },
   resolve: {
     extensions: ['*', '.webpack.js', '.web.js', '.ts', '.js', '.scss', '.css', '.ejs']
   },
   plugins: [
-    new CleanWebpackPlugin('./dist'),
     new HtmlWebpackPlugin({
       title: 'SchunterKino Fernbedienung',
       template: 'app/html/frontend',
@@ -35,7 +32,7 @@ module.exports = {
       description: 'Webapp zur Bedienung der Kinotechnik',
       theme_color: '#fef400',
       background_color: '#fef400',
-      start_url: "frontend.html",
+      start_url: 'frontend.html',
       ios: true,
       icons: [{
         src: path.resolve('app/ic_launcher.png'),
@@ -50,26 +47,32 @@ module.exports = {
     new ExtractTextPlugin('styles.css'),
   ],
   module: {
-    rules: [{
-      test: /\.ts$/,
-      use: 'ts-loader'
-    },
-    {
-      test: /\.s?css$/,
-      use: ExtractTextPlugin.extract({
-        use: [
-          { loader: "css-loader", options: { sourceMap: true } },
-          { loader: "postcss-loader", options: { sourceMap: true, plugins: () => [autoprefixer] } },
-          { loader: "sass-loader", options: { sourceMap: true } },
-        ],
-        fallback: [
-          { loader: "style-loader", options: { sourceMap: true } }
-        ]
-      })
-    },
-    {
-      test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$|.svg$/,
-      loader: 'url-loader'
-    }]
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        use: { loader: 'tslint-loader' }
+      },
+      {
+        test: /\.ts$/,
+        use: { loader: 'ts-loader' }
+      },
+      {
+        test: /\.s?css$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            { loader: 'css-loader', options: { sourceMap: true } },
+            { loader: 'postcss-loader', options: { sourceMap: true, plugins: () => [autoprefixer] } },
+            { loader: 'sass-loader', options: { sourceMap: true } },
+          ],
+          fallback: [
+            { loader: 'style-loader', options: { sourceMap: true } }
+          ]
+        })
+      },
+      {
+        test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$|.svg$/,
+        loader: 'url-loader'
+      }]
   }
-};
+}
