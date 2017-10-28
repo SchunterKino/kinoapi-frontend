99d1e359a1fd536ace6d1572edf7ebb6cf5c5e41
Mike Klimek <m.klimek@tu-bs.de>
2017-10-29 00:17:13 +0200

    Differentiate between first login and incorrect user/password

--- app/js/dialog/login.ts
+++ app/js/dialog/login.ts
@@ -3,15 +3,15 @@ const dialog = $(`
     <div class="modal-dialog modal-m">
       <div class="modal-content">
         <div class="modal-header">
-          <h3 class="dialog-message">Bitte anmelden</h3>
+          <h3 class="dialog-message"></h3>
         </div>
         <div class="modal-body">
           <form class="form-inline">
-            <div class="form-group">
+            <div class="form-group user-group">
               <label class="sr-only" for="user">Benutzer</label>
               <input type="text" class="form-control" id="user" placeholder="Benutzer">
             </div>
-            <div class="form-group">
+            <div class="form-group password-group">
               <label class="sr-only" for="password">Passwort</label>
               <input type="password" class="form-control" id="password" placeholder="Passwort">
             </div>
@@ -23,9 +23,16 @@ const dialog = $(`
   </div>
 `);
 
+const message = "Bitte anmelden";
+const failedMessage = "Name oder Kennwort ist nicht korrekt";
 let loginCallback;
 export default {
-  show: () => dialog.modal("show"),
+  show: (error: boolean) => {
+    dialog.find(".dialog-message").text(error ? failedMessage : message);
+    dialog.find(".user-group").toggleClass("has-error", error);
+    dialog.find(".password-group").toggleClass("has-error", error);
+    dialog.modal("show");
+  },
   hide: () => dialog.modal("hide"),
   onLogin: (callback) => loginCallback = callback
 };
@@ -34,4 +41,9 @@ dialog.find("button").click(() => {
   const user = dialog.find("#user").val();
   const password = dialog.find("#password").val();
   loginCallback(user, password);
+});
+
+dialog.find("#user").keyup(() => {
+  dialog.find(".user-group").removeClass("has-error");
+  dialog.find(".password-group").removeClass("has-error");
 });
