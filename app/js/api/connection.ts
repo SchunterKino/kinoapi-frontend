2e0f3a1e0bc44440ac3afa3e252720d6b2fb6a4f
Mike Klimek <m.klimek@tu-bs.de>
2017-10-29 01:18:33 +0200

    Use ajax post to log in with a password (no username)

--- app/js/api/connection.ts
+++ app/js/api/connection.ts
@@ -1,56 +1,87 @@
-import token from "./token";
+const websocketUrl = new URL("wss://remote.schunterkino.de:8641");
+const tokenServerUrl = new URL("https://remote.schunterkino.de:1234"); // FIXME
+export class Connection {
+  private callbacks: { [type: string]: (data) => void } = {};
+  private socket: WebSocket;
+  private openCallback: () => void;
+  private closeCallback: () => void;
+  private errorCallback: (error: string) => void;
+  private unauthorizedCallback: () => void;
 
-const callbacks = {}; // type - callback
-let socket;
-let openCallback;
-let closeCallback;
-let errorCallback;
-let unauthorizedCallback;
-export default {
-  onmessage: (type, callback) => callbacks[type] = callback,
-  send: (msg) => socket.send(msg),
-  onClose: (callback) => closeCallback = callback,
-  onOpen: (callback) => openCallback = callback,
-  onError: (callback) => errorCallback = callback,
-  onUnauthorized: (callback) => unauthorizedCallback = callback,
-  connect
-};
+  public constructor(private url: URL) { }
 
-function connect(user?, password?) {
-  console.log("WS connecting...");
-  let userString = "";
-  if (typeof user !== "undefined" && typeof password !== "undefined") {
-    userString = user + ":" + password + "@";
+  // TODO use rxjs for callbacks?
+  public onmessage(type: string, callback: (data) => void) {
+    this.callbacks[type] = callback;
   }
-  socket = new WebSocket("wss://" + userString + "remote.schunterkino.de:8641");
 
-  socket.onopen = () => {
-    console.log("WS opened");
-    openCallback();
-  };
+  public send(msg: string) {
+    this.socket.send(msg);
+  }
 
-  socket.onclose = (evt) => {
-    console.log("WS closed");
-    closeCallback();
-    if (evt.code === 4401) {
-      token.delete();
-      unauthorizedCallback();
-    } else {
-      // try to reconnect
-      setTimeout(() => connect(), 2000);
-    }
-  };
+  public onClose(callback: () => void) {
+    this.closeCallback = callback;
+  }
 
-  // allow registering multiple handlers
-  socket.onmessage = (evt) => {
-    console.log(evt);
-    const data = JSON.parse(evt.data);
-    if (data.msg_type === "error") {
-      errorCallback(data.error);
-    } else if (data.msg_type === "authorization") {
-      token.save(data.token);
-    } else if (data.msg_type in callbacks) {
-      callbacks[data.msg_type](data);
-    }
-  };
+  public onOpen(callback: () => void) {
+    this.openCallback = callback;
+  }
+
+  public onError(callback: (error: string) => void) {
+    this.errorCallback = callback;
+  }
+
+  public onUnauthorized(callback: () => void) {
+    this.unauthorizedCallback = callback;
+  }
+
+  public login(password: string) {
+    $.ajax({
+      method: "POST",
+      url: tokenServerUrl.toString(),
+      data: { password }
+    })
+      .done(() => {
+        this.connect();
+      })
+      .fail((jqXHR, textStatus, error) => {
+        this.unauthorizedCallback();
+        if (jqXHR.status !== 401) {
+          this.errorCallback(jqXHR.responseText);
+        }
+      });
+  }
+
+  public connect() {
+    console.log("WS connecting...");
+    this.socket = new WebSocket(this.url.toString());
+
+    this.socket.onopen = () => {
+      console.log("WS opened");
+      this.openCallback();
+    };
+
+    this.socket.onclose = (evt) => {
+      console.log("WS closed");
+      this.closeCallback();
+      if (evt.code === 4401) {
+        this.unauthorizedCallback();
+      } else {
+        // try to reconnect
+        setTimeout(() => this.connect(), 2000);
+      }
+    };
+
+    this.socket.onmessage = (evt) => {
+      console.log(evt);
+      const data = JSON.parse(evt.data);
+      if (data.msg_type === "error") {
+        this.errorCallback(data.error);
+      } else if (data.msg_type in this.callbacks) {
+        this.callbacks[data.msg_type](data);
+      }
+    };
+  }
 }
+
+export default new Connection(websocketUrl);
--- app/js/dialog/login.ts
+++ app/js/dialog/login.ts
@@ -7,10 +7,6 @@ const dialog = $(`
         </div>
         <div class="modal-body">
           <form class="form-inline">
-            <div class="form-group user-group">
-              <label class="sr-only" for="user">Benutzer</label>
-              <input type="text" class="form-control" id="user" placeholder="Benutzer">
-            </div>
             <div class="form-group password-group">
               <label class="sr-only" for="password">Passwort</label>
               <input type="password" class="form-control" id="password" placeholder="Passwort">
@@ -29,7 +25,6 @@ let loginCallback;
 export default {
   show: (error: boolean) => {
     dialog.find(".dialog-message").text(error ? failedMessage : message);
-    dialog.find(".user-group").toggleClass("has-error", error);
     dialog.find(".password-group").toggleClass("has-error", error);
     dialog.modal("show");
   },
@@ -38,12 +33,10 @@ export default {
 };
 
 dialog.find("button").click(() => {
-  const user = dialog.find("#user").val();
   const password = dialog.find("#password").val();
-  loginCallback(user, password);
+  loginCallback(password);
 });
 
-dialog.find("#user").keyup(() => {
-  dialog.find(".user-group").removeClass("has-error");
+dialog.find("#password").keyup(() => {
   dialog.find(".password-group").removeClass("has-error");
 });
