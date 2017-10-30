c4376b62685e50ede60720a489dcabcbd8d9a5ae
Mike Klimek <m.klimek@tu-bs.de>
2017-10-30 15:35:42 +0100

    Fetch from network first and fallback to cache

--- app/js/notify/sw.ts
+++ app/js/notify/sw.ts
@@ -20,7 +20,7 @@ let assetsToCache = [...assets, "./"];
 assetsToCache = assetsToCache.map((path) => new URL(path, global.location).toString());
 
 self.addEventListener("install", (event) => {
-    // become active immediately, even if old service-wroker exists
+    // become active immediately, even if old service-worker exists
     self.skipWaiting();
     // cache assets
     event.waitUntil(
@@ -58,39 +58,26 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    const request = event.request;
-    const requestUrl = new URL(request.url);
-    const resource = global.caches.match(request).then((response) => {
-        // try cache
-        if (response) {
-            console.log("[SW] fetched from cache", requestUrl.href);
-            return response;
-        }
-
-        // try network
-        return fetch(request)
-            .then((responseNetwork) => {
-                if (!responseNetwork || !responseNetwork.ok) {
-                    console.error("[SW] invalid response", responseNetwork);
-                    return responseNetwork;
-                }
-                console.log("[SW] fetched from network", requestUrl.href);
-                // cache response
-                const responseCache = responseNetwork.clone();
-                global.caches
-                    .open(CACHE_NAME)
-                    .then((cache) => {
-                        return cache.put(request, responseCache);
-                    });
-                return responseNetwork;
-            })
-            .catch(() => {
-                // user is landing on our page
-                if (event.request.mode === "navigate") {
-                    return global.caches.match("./");
-                }
-                return null;
+    const url = new URL(event.request.url);
+    event.respondWith(
+        fetch(event.request).then((response) => {
+            // network first
+            if (response && response.ok) {
+                return response;
+            } else {
+                throw new Error("Invalid response");
+            }
+        }).then((response) => {
+            // cache response
+            console.log("[SW] fetched from network", url.href);
+            return caches.open(CACHE_NAME).then((cache) => {
+                cache.put(event.request, response.clone());
+                return response;
             });
-    });
-    event.respondWith(resource);
+        }).catch(() => {
+            // fallback to cache
+            console.log("[SW] fetched from cache", url.href);
+            return caches.match(event.request);
+        })
+    );
 });
